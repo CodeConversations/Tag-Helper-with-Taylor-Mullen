f9ec9117cd4e534083a1f86cd81bc08eefca7f6e
N. Taylor Mullen <nimullen@microsoft.com>
2017-04-10 22:03:11 -0700

    Added markdown TagHelper

---
+++ CreatingYourOwnTagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonMark;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CreatingYourOwnTagHelpers
+{
+    [HtmlTargetElement(Attributes = "markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        public string Suffix { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+            var lines = childContent.GetContent()
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Trim());
+            var content = string.Join(" ", lines);
+            var transformedContent = CommonMarkConverter.Convert(content);
+
+            output.Content.SetHtmlContent(transformedContent);
+            output.PostContent.SetHtmlContent(Suffix);
+
+            output.Attributes.RemoveAll("markdown");
+        }
+    }
+}
